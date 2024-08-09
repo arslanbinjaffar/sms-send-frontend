58a92276a21d36b727637a486bbe0e29791682ce
Arslan Jaffar <106986031+arslanbinjaffar@users.noreply.github.com>
2024-08-09 08:40:02 +0000

    ....

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:8000/";
-// axios.defaults.baseURL="https://izhmw2qjmx.us-east-2.awsapprunner.com/"
+// axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.baseURL = "https://izhmw2qjmx.us-east-2.awsapprunner.com/";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
