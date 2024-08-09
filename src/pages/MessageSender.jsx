6883d630899fd55a847c028bb7441efabf5a831b
Arslan Jaffar <106986031+arslanbinjaffar@users.noreply.github.com>
2024-08-09 08:52:25 +0000

    ...

--- src/pages/MessageSender.jsx
+++ src/pages/MessageSender.jsx
@@ -9,10 +9,10 @@ const MessageSender = () => {
   const [fileUpload, setFileUpload] = useState([]);
   const [groups, setGroups] = useState([]);
   const [message, setMessage] = useState("");
-  const [senderNum,setSenderNum]=useState("")
-  const [page, setPage] = useState(1)
+  const [senderNum, setSenderNum] = useState("");
+  const [page, setPage] = useState(1);
   const [processing, setProcessing] = useState(false);
-  const [isSubmitted,setIsSubmitted]=useState(false)
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const handleFileChange = (event) => {
     const selectedFile = event.target.files[0];
     setFileUpload(selectedFile);
@@ -27,41 +27,42 @@ const MessageSender = () => {
       setIsSubmitted(true);
       const url = await uploadImage(fileUpload);
       toast.success("File uploaded successfully");
-      const res = await axios.post('/api/v1/sms/process', { url });
+      const res = await axios.post("/api/v1/sms/process", { url });
       toast.success("Data sent successfully");
       setIsSubmitted(false);
-      console.log(res,"res")
+      console.log(res, "res");
       setGroups(res.data?.data);
     } catch (error) {
       toast.error("File upload failed: " + error.message);
       setIsSubmitted(false);
     }
   };
- 
 
   const handlelogOut = async () => {
-    const token = localStorage.removeItem("acesss_token")
+    const token = localStorage.removeItem("acesss_token");
     if (!token) {
-    navigate("/login");
+      navigate("/login");
     }
   };
-  const FetchGroupsData = async (page,limit=5) => {
+  const FetchGroupsData = async (page, limit = 5) => {
     try {
-      setProcessing(true)
-      const res = await axios.get(`/api/v1/sms/group?page=${page}&limit=${limit}`);
-      setPage(page)
-      setProcessing(false)
+      setProcessing(true);
+      const res = await axios.get(
+        `/api/v1/sms/group?page=${page}&limit=${limit}`
+      );
+      setPage(page);
+      setProcessing(false);
       setGroups(res.data?.data);
     } catch (error) {
-      setProcessing(false)
-      console.log(error)
+      setProcessing(false);
+      console.log(error);
     }
-  }
+  };
   useEffect(() => {
     FetchGroupsData(page);
   }, []);
-  console.log(groups)
-  console.log(page)
+  console.log(groups);
+  console.log(page);
   const memoizedComponent = useMemo(() => {
     return (
       <section className="bg-gray-50">
@@ -77,51 +78,51 @@ const MessageSender = () => {
                   className=" text-white bg-red-500  font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 >
                   Logout
-                </button> 
+                </button>
               </div>
-                <div className="flex items-center justify-center w-full flex-col gap-4">
-                    <label
-                      htmlFor="dropzone-file"
-                      className="flex flex-col items-center justify-center w-full h-28 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+              <div className="flex items-center justify-center w-full flex-col gap-4">
+                <label
+                  htmlFor="dropzone-file"
+                  className="flex flex-col items-center justify-center w-full h-28 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100"
+                >
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg
+                      className="w-8 h-8 mb-4 text-gray-500"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 16"
                     >
-                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                        <svg
-                          className="w-8 h-8 mb-4 text-gray-500"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 20 16"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                          />
-                        </svg>
-                        <p className="mb-2 text-sm text-gray-500">
-                          <span className="font-semibold">Click to upload</span>{" "}
-                          or drag and drop
-                        </p>
-                        <p className="text-xs text-gray-500">CSV File</p>
-                      </div>
-                      <input
-                        id="dropzone-file"
-                        type="file"
-                        className="hidden"
-                        onChange={handleFileChange}
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                       />
-                    </label>
-                  {fileUpload.length !== 0 && <h1>File::{fileUpload?.name}</h1>}
+                    </svg>
+                    <p className="mb-2 text-sm text-gray-500">
+                      <span className="font-semibold">Click to upload</span> or
+                      drag and drop
+                    </p>
+                    <p className="text-xs text-gray-500">CSV File</p>
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    className="hidden"
+                    onChange={handleFileChange}
+                  />
+                </label>
+                {fileUpload.length !== 0 && <h1>File::{fileUpload?.name}</h1>}
                 <button
-                    disabled={isSubmitted}
-                    onClick={handleUploadFile}
-                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                  >
-                    Submit 
-                  </button>
-                </div>
+                  disabled={isSubmitted}
+                  onClick={handleUploadFile}
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                >
+                  Submit
+                </button>
+              </div>
               {/* <div>
                 <label
                   htmlFor="number-input"
@@ -138,8 +139,18 @@ const MessageSender = () => {
                   required
                 />
               </div> */}
-              {isSubmitted && <div className="flex justify-center items-center"> <Loader /></div>}
-              {processing && <div className="flex justify-center items-center"> <Loader /></div>}
+              {isSubmitted && (
+                <div className="flex justify-center items-center">
+                  {" "}
+                  <Loader />
+                </div>
+              )}
+              {processing && (
+                <div className="flex justify-center items-center">
+                  {" "}
+                  <Loader />
+                </div>
+              )}
               {groups.length !== 0 && (
                 <>
                   <div>
@@ -168,7 +179,7 @@ const MessageSender = () => {
                     </label>
                     <input
                       type="text"
-                      onChange={(e) =>setSenderNum(Number(e.target.value))}
+                      onChange={(e) => setSenderNum(Number(e.target.value))}
                       value={senderNum}
                       id="first_name"
                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -176,7 +187,16 @@ const MessageSender = () => {
                       required
                     />
                   </div>
-                  <Groups groups={groups} message={message} setGroups={setGroups} FetchGroupsData={FetchGroupsData} page={page} setPage={setPage} setProcessing={ setProcessing} processing={ processing} />
+                  <Groups
+                    groups={groups}
+                    message={message}
+                    setGroups={setGroups}
+                    FetchGroupsData={FetchGroupsData}
+                    page={page}
+                    setPage={setPage}
+                    setProcessing={setProcessing}
+                    processing={processing}
+                  />
                 </>
               )}
             </div>
@@ -184,7 +204,7 @@ const MessageSender = () => {
         </div>
       </section>
     );
-  }, [fileUpload, message, processing,isSubmitted,page]);
+  }, [fileUpload, message, processing, isSubmitted, page, senderNum]);
   return memoizedComponent;
 };
 
@@ -214,13 +234,22 @@ function Loader() {
   );
 }
 
-function Groups({ groups,setGroups,message, page, setPage, processing, setProcessing }) {
-
+function Groups({
+  groups,
+  setGroups,
+  message,
+  page,
+  setPage,
+  processing,
+  setProcessing,
+}) {
   const loadMoreData = async () => {
     if (!processing) {
       try {
         setProcessing(true);
-        const res = await axios.get(`/api/v1/sms/group?page=${page + 1}&limit=20`);
+        const res = await axios.get(
+          `/api/v1/sms/group?page=${page + 1}&limit=20`
+        );
         setPage(page + 1);
         setProcessing(false);
         setGroups((prevGroups) => [...prevGroups, ...res.data?.data]);
@@ -283,7 +312,7 @@ function Groups({ groups,setGroups,message, page, setPage, processing, setProces
               focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5
               py-2.5 text-center`}
           >
-            {processing ? 'Loading...' : 'Load More'}
+            {processing ? "Loading..." : "Load More"}
           </button>
         </div>
       )}
